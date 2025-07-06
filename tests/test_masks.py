334f4bfc8f0ae65135b8352612e8661fd8bbf176
Kirill-Leschinskiy <kirenek@yandex.ru>
2025-07-06 18:31:35 +0300

    Changes in module "masks"

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,14 +1,18 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
-@pytest.mark.parametrize('card_number, expected',[('1234567890123456', '1234 56** **** 3456'),
-                                                  ('7893567873803481', '7893 56** **** 3481')])
+
+@pytest.mark.parametrize(
+    "card_number, expected", [("1234567890123456", "1234 56** **** 3456"), ("7893567873803481", "7893 56** **** 3481")]
+)
 def test_get_mask_card_number(card_number, expected):
     assert get_mask_card_number(card_number) == expected
 
 
-def test_raises_get_mask_card_number(invalid_data_int, invalid_data_list, invalid_data_empty_string, invalid_data_few_digits, many_digits):
+def test_raises_get_mask_card_number(
+    invalid_data_int, invalid_data_list, invalid_data_empty_string, invalid_data_few_digits, many_digits
+):
     with pytest.raises(ValueError):
         get_mask_card_number(invalid_data_int)
     with pytest.raises(ValueError):
@@ -21,13 +25,16 @@ def test_raises_get_mask_card_number(invalid_data_int, invalid_data_list, invali
         get_mask_card_number(many_digits)
 
 
-@pytest.mark.parametrize('account_number, expected',[('12345678901234564356', '**4356'),
-                                                  ('78935678738034823521', '**3521')])
+@pytest.mark.parametrize(
+    "account_number, expected", [("12345678901234564356", "**4356"), ("78935678738034823521", "**3521")]
+)
 def test_get_mask_account(account_number, expected):
     assert get_mask_account(account_number) == expected
 
 
-def test_raises_get_mask_account(invalid_data_int, invalid_data_list, invalid_data_empty_string, few_digits_for_account_number, many_digits):
+def test_raises_get_mask_account(
+    invalid_data_int, invalid_data_list, invalid_data_empty_string, few_digits_for_account_number, many_digits
+):
     with pytest.raises(ValueError):
         get_mask_card_number(invalid_data_int)
     with pytest.raises(ValueError):
